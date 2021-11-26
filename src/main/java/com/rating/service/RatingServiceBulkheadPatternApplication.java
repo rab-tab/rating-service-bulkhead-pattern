e79f1b7e9bb14eb60c46846f437eb3b8ea15881f
Rabia <sdetcode@gmail.com>
2021-11-27 00:30:06 +0530

    rating service

---
+++ src/main/java/com/rating/service/RatingServiceBulkheadPatternApplication.java
@@ -0,0 +1,13 @@
+package com.rating.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingServiceBulkheadPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingServiceBulkheadPatternApplication.class, args);
+	}
+
+}
